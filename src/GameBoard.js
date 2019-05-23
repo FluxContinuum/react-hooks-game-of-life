e2707e91eacba26848e51641d624725907bf6a92
fluxcontinuum <jordan@fluxcontinuum.com>
2019-05-23 08:11:54 -0500

    Cleanup

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -2,32 +2,18 @@ import React, { useState, useEffect } from 'react';
 import './GameBoard.css';
 import GameCell from './GameCell';
 
-const createGrid = (rows, cols) => {
+const createGrid = (rows, cols, isRandom=false) => {
 	let grid = [];
 
 	for (let x = 0; x < cols; x++){
 		grid[x] = [];
 
 		for (let y = 0; y < rows; y++){
-			grid[x][y] = false;
+			grid[x][y] = isRandom	? (Math.random() >= 0.5) : false;
 		}
 	}
 
 	return grid;
-}
-
-const createRandom = (rows, cols) => {
-	let randomGrid = [];
-	
-	for (let x = 0; x < cols; x++){
-		randomGrid[x] = [];
-
-		for (let y = 0; y < rows; y++){
-			randomGrid[x][y] = Math.random() >= 0.5;
-		}
-	}
-
-	return randomGrid;
 }
 
 const createCells = grid => {
@@ -69,14 +55,14 @@ const evalNeighbors = (x, y, grid) => {
 function GameBoard() {
 	const [width, setWidth] = useState(800);
 	const [height, setHeight] = useState(600);
-	const [cellSize, setCellSize] = useState(5);
+	const [cellSize, setCellSize] = useState(10);
 	const [running, setRunning] = useState(false);
 	const [gameInterval, setGameInterval] = useState(100);
 
 	const rows = height / cellSize;
 	const cols = width / cellSize;
 
-	const [grid, setGrid] = useState(createRandom(rows, cols));
+	const [grid, setGrid] = useState(createGrid(rows, cols, true));
 	const [cells, setCells] = useState(createCells(grid));
 
 	useEffect(() => {
