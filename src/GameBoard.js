c10d628c1c55a49c6961cfda06469cb5024cac90
fluxcontinuum <jordan@fluxcontinuum.com>
2019-05-27 17:58:25 -0500

    Eliminate redundant renders with useReducer

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,12 +1,6 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useReducer } from 'react';
 import './GameBoard.css';
 import GameCell from './GameCell';
-
-const WIDTH = 1200;
-const HEIGHT = 600;
-const CELL_SIZE = 5;
-const ROWS = HEIGHT / CELL_SIZE;
-const COLS = WIDTH / CELL_SIZE;
 
 const createGrid = (rows, cols, isRandom=false) => {
 	let grid = [];
@@ -23,19 +17,6 @@ const createGrid = (rows, cols, isRandom=false) => {
 }
 
 const createCells = grid => {
-/*	const rows = grid.length;
-	const cols = grid[0].length;
-
-	let cells = [];
-
-	for (let y = 0; y < rows; y++){
-		for (let x = 0; x < cols; x++){
-			if (grid[y][x]){
-				cells.push({ x, y });
-			}
-		}
-	}
-*/
 	const cells = grid.flatMap((row, y) => {
 		return row.reduce((living, col, x) => {
 			if (col){
@@ -67,59 +48,78 @@ const evalNeighbors = (x, y, grid) => {
 	return neighbors;
 }
 
+const reducer = (state, action) => {
+	switch(action.type) {
+		case 'UPDATE_GAME':
+			return {...state, grid: action.grid, cells: createCells(action.grid)}
+		case 'TOGGLE_RUNNING':
+			return {...state, isRunning: !state.isRunning}
+		case 'SET_INTERVAL':
+			return {...state, gameInterval: action.gameInterval}
+		default:
+			throw new Error();
+	}
+}
+
+const WIDTH = 1200;
+const HEIGHT = 600;
+const CELL_SIZE = 5;
+const ROWS = HEIGHT / CELL_SIZE;
+const COLS = WIDTH / CELL_SIZE;
+
+const initGrid = createGrid(ROWS, COLS, true);
+const initState = {
+	grid: initGrid,
+	cells: createCells(initGrid),
+	isRunning: false,
+	gameInterval: 100
+}
+
 function GameBoard() {
-	const [running, setRunning] = useState(false);
-	const [gameInterval, setGameInterval] = useState(100);
-
-	const [grid, setGrid] = useState(createGrid(ROWS, COLS, true));
-	const [cells, setCells] = useState(createCells(grid));
-
-	// set cells when grid updates
-	useEffect(() => {
-		setCells(createCells(grid));
-	}, [grid]);
+	const [state, dispatch] = useReducer(reducer, initState);
 
 	// main game loop
 	useEffect(() => {
-		if (running){
+		if (state.isRunning){
 			let newGrid = createGrid(ROWS, COLS);
 
 			for (let y = 0; y < ROWS; y++){
 				for (let x = 0; x < COLS; x++){
-					const neighbors = evalNeighbors(x, y, grid);
+					const neighbors = evalNeighbors(x, y, state.grid);
 
-					if (grid[y][x]){
+					if (state.grid[y][x]){
 						if (neighbors === 2 || neighbors === 3){
 							newGrid[y][x] = true;
 						}else{
 							newGrid[y][x] = false;
 						}
-					}else if(!grid[y][x] && neighbors === 3){
+					}else if(!state.grid[y][x] && neighbors === 3){
 						newGrid[y][x] = true;
 					}
 				}
 			}
 
 			const timeout = setTimeout(() => {
-				setGrid(newGrid);
-			}, gameInterval);
+				dispatch({type: 'UPDATE_GAME', grid: newGrid});
+			}, state.gameInterval);
 
 			return () => {
 				clearTimeout(timeout);
 			}
 		}
-	}, [grid, gameInterval, running]);
+	}, [state.grid, state.gameInterval, state.isRunning]);
 
 	return (
     <div>
       <div className="game-board" style={{ width: WIDTH, height: HEIGHT, backgroundSize: `${CELL_SIZE}px ${CELL_SIZE}px`}} >
-        { cells.map(cell => <GameCell size={CELL_SIZE} x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`} />) }
+        { state.cells.map(cell => <GameCell size={CELL_SIZE} x={cell.x} y={cell.y} key={`${cell.x},${cell.y}`} />) }
       </div>
       <div>
-      	<span style={{fontSize: 'calc(10px + 1vmin)'}}>Game Interval:</span> <input value={gameInterval} onChange={ev => setGameInterval(ev.target.value)} />
-      	<button className="button-control" onClick={() => setRunning(!running)}> {running ? 'Stop' : 'Start'} </button>
-      	<button className="button-control" onClick={() => setGrid(createGrid(ROWS, COLS, true))}> Randomize </button>
-      	<button className="button-control" onClick={() => setGrid(createGrid(ROWS, COLS))}> Empty </button>
+      	<span style={{fontSize: 'calc(10px + 1vmin)'}}>Game Interval:</span>
+      	<input style={{marginLeft: '5px'}} value={state.gameInterval} onChange={ev => dispatch({type: 'SET_INTERVAL', gameInterval: ev.target.value})} />
+      	<button className="button-control" onClick={() => dispatch({type: 'TOGGLE_RUNNING', isRunning: !state.isRunning})}> {state.isRunning ? 'Stop' : 'Start'} </button>
+      	<button className="button-control" onClick={() => dispatch({type: 'UPDATE_GAME', grid: createGrid(ROWS, COLS, true)})}> Randomize </button>
+      	<button className="button-control" onClick={() => dispatch({type: 'UPDATE_GAME', grid: createGrid(ROWS, COLS)})}> Empty </button> 
       </div>
     </div>
 	);
